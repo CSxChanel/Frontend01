4049b1a799006b931f3d2f9e849c1abcfe0b7bf8
Cp_Sudrajat <120399183+CSxChanel@users.noreply.github.com>
2025-04-13 09:37:37 +0700

    Update auth-login.js

--- js/auth-login.js
+++ js/auth-login.js
@@ -19,7 +19,7 @@ form.addEventListener("submit", async e => {
         form.reset();
         // redirect ke dashboard
         setTimeout(() => {
-            window.location.href = "/Frontend01/pages/dashboard.html?page=dashboard";
+            window.location.href = "/Frontend01/dashboard.html?page=dashboard";
 
         }, 1000);
     } else {
